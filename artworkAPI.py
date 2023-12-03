0a0e59e1445e7ca9680f446e09ab7583aa77c999
Luke Salamat <150443651+Luke-Salamat@users.noreply.github.com>
2023-12-03 21:19:43 +0800

    completed

--- artworkAPI.py
+++ artworkAPI.py
@@ -6,13 +6,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
 bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///artwork.db'
-app.config['SECRET_KEY'] = 'Secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("database")
+app.config['SECRET_KEY'] = os.getenv("secretkey")
 db = SQLAlchemy(app)
+
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -174,116 +180,136 @@ def register():
 class postArtwork(Resource):   
     @marshal_with(resource_fields)
     def post(self, art_id):
+        key = os.getenv('api_key')
+        api_key = request.headers.get('Authorization')
         args = post_artwork.parse_args()
         artExist = ArtworkModel.query.filter_by(id=art_id).first()
-        if artExist:
-            abort(409, message="Artwork id taken")
-            
-        if args['provenance'] not in ('true', 'false'):
-            abort(400, message="Invalid provenance value") 
-            
-        if args['category'] not in ('painting', 'sculpture', 'drawing'):
-            abort(400, message="Invalid category value")  
-            
-        if args['color'] not in ('warm', 'cool', 'complementary'):
-            abort(400, message="Invalid color value")       
-        newArt = ArtworkModel(id=art_id, category=args['category'], title=args['title'], artist=args['artist'], artistOrigin=args['artistOrigin'], provenance=args['provenance'], medium=args['medium'], completionDate=args['completionDate'], era=args['era'], color=args['color'], dimensions=args['dimensions'], location=args['location'], image=args['image'], startingbid=args['startingbid'])
-        db.session.add(newArt)
-        db.session.commit()
-        return newArt, 201
+        if api_key != key:
+            abort(401, message="Invalid API key")
+        else:
+            if artExist:
+                abort(409, message="Artwork id taken")
+                
+            if args['provenance'] not in ('true', 'false'):
+                abort(400, message="Invalid provenance value") 
+                
+            if args['category'] not in ('painting', 'sculpture', 'drawing'):
+                abort(400, message="Invalid category value")  
+                
+            if args['color'] not in ('warm', 'cool', 'complementary', 'mixed'):
+                abort(400, message="Invalid color value")       
+            newArt = ArtworkModel(id=art_id, category=args['category'], title=args['title'], artist=args['artist'], artistOrigin=args['artistOrigin'], provenance=args['provenance'], medium=args['medium'], completionDate=args['completionDate'], era=args['era'], color=args['color'], dimensions=args['dimensions'], location=args['location'], image=args['image'], startingbid=args['startingbid'])
+            db.session.add(newArt)
+            db.session.commit()
+            return newArt, 201
                      
 class getArtwork(Resource):
     @marshal_with(resource_fields)
     def get(self):
+        key = os.getenv('api_key')
+        api_key = request.headers.get('Authorization')
         art_id = request.args.get('id')
         artist = request.args.get('artist')
         era = request.args.get('era')
         color = request.args.get('color')
         category = request.args.get('category')
 
-        if art_id:
-            artwork = ArtworkModel.query.get(art_id)
-            if not artwork:
-                abort(404, message="Artwork id doesn't exist")
-            return artwork,200
-        elif category:
-            artwork = ArtworkModel.query.filter_by(category=category).all()
-            if not artwork:
-                abort(404, message="Artwork category doesn't exist")
-            return artwork, 200
-        elif artist:
-            artwork = ArtworkModel.query.filter(ArtworkModel.artist.contains(artist)).all()
-            if not artwork:
-                abort(404, message="Artwork artist doesn't exist")
-            return artwork, 200
-        elif era:
-            artwork = ArtworkModel.query.filter(ArtworkModel.era.contains(era)).all()
-            if not artwork:
-                abort(404, message="Artwork era doesn't exist")
-            return artwork, 200
-        elif color:
-            artwork = ArtworkModel.query.filter(ArtworkModel.color.contains(color)).all()
-            if not artwork:
-                abort(404, message="Artwork color doesn't exist")
-            return artwork, 200
+        if api_key != key:
+            abort(401, message="Invalid API key")
         else:
-            collections = ArtworkModel.query.all()
-            artworks = {}
-            for artwork in collections:
-                artworks[artwork] = {"id": artwork.id, "title": artwork.title, "artist": artwork.artist, "artistOrigin": artwork.artistOrigin, "provenance": artwork.provenance, "medium": artwork.medium, "completionDate": artwork.completionDate, "era": artwork.era, "color": artwork.color, "dimensions": artwork.dimensions, "location": artwork.location, "image": artwork.image, "startingbid": artwork.startingbid}
-            return collections
+            if art_id:
+                artwork = ArtworkModel.query.get(art_id)
+                if not artwork:
+                    abort(404, message="Artwork id doesn't exist")
+                return artwork,200
+            elif category:
+                artwork = ArtworkModel.query.filter_by(category=category).all()
+                if not artwork:
+                    abort(404, message="Artwork category doesn't exist")
+                return artwork, 200
+            elif artist:
+                artwork = ArtworkModel.query.filter(ArtworkModel.artist.contains(artist)).all()
+                if not artwork:
+                    abort(404, message="Artwork artist doesn't exist")
+                return artwork, 200
+            elif era:
+                artwork = ArtworkModel.query.filter(ArtworkModel.era.contains(era)).all()
+                if not artwork:
+                    abort(404, message="Artwork era doesn't exist")
+                return artwork, 200
+            elif color:
+                artwork = ArtworkModel.query.filter(ArtworkModel.color.contains(color)).all()
+                if not artwork:
+                    abort(404, message="Artwork color doesn't exist")
+                return artwork, 200
+            else:
+                collections = ArtworkModel.query.all()
+                artworks = {}
+                for artwork in collections:
+                    artworks[artwork] = {"id": artwork.id, "title": artwork.title, "artist": artwork.artist, "artistOrigin": artwork.artistOrigin, "provenance": artwork.provenance, "medium": artwork.medium, "completionDate": artwork.completionDate, "era": artwork.era, "color": artwork.color, "dimensions": artwork.dimensions, "location": artwork.location, "image": artwork.image, "startingbid": artwork.startingbid}
+                return collections
         
 class putArtwork(Resource):
     @marshal_with(resource_fields)
     def put(self, art_id):
+        key = os.getenv('api_key')
+        api_key = request.headers.get('Authorization')
         artwork = ArtworkModel.query.get(art_id)
-        if not artwork:
-            abort(404, message="Artwork with given id does not exist")
-        
-        update_data = request.json  
-        if not update_data:
-            abort(400, message="No update data provided")
- 
-        if 'title' in update_data:
-            artwork.title = update_data['title']
-        if 'artist' in update_data:
-            artwork.artist = update_data['artist']
-        if 'artistOrigin' in update_data:
-            artwork.artistOrigin = update_data['artistOrigin']
-        if 'provenance' in update_data:
-            artwork.provenance = update_data['provenance']
-        if 'medium' in update_data:
-            artwork.medium = update_data['medium']    
-        if 'completionDate' in update_data:
-            artwork.comepletionDate = update_data['completionDate']
-        if 'color' in update_data:
-            artwork.color = update_data['color']
-        if 'era' in update_data:
-            artwork.era = update_data['era']
-        if 'dimensions' in update_data:
-            artwork.dimensions = update_data['dimensions']
-        if 'location' in update_data:
-            artwork.location = update_data['location']
-        if 'image' in update_data:
-            artwork.image = update_data['image']
-        if 'startingbid' in update_data:
-            artwork.startingbid = update_data['startingbid']
-        db.session.commit()
-        return artwork, 200
+        if api_key != key:
+            abort(401, message="Invalid API key")
+        else:
+            if not artwork:
+                abort(404, message="Artwork with given id does not exist")
+            
+            update_data = request.json  
+            if not update_data:
+                abort(400, message="No update data provided")
+    
+            if 'title' in update_data:
+                artwork.title = update_data['title']
+            if 'artist' in update_data:
+                artwork.artist = update_data['artist']
+            if 'artistOrigin' in update_data:
+                artwork.artistOrigin = update_data['artistOrigin']
+            if 'provenance' in update_data:
+                artwork.provenance = update_data['provenance']
+            if 'medium' in update_data:
+                artwork.medium = update_data['medium']    
+            if 'completionDate' in update_data:
+                artwork.comepletionDate = update_data['completionDate']
+            if 'color' in update_data:
+                artwork.color = update_data['color']
+            if 'era' in update_data:
+                artwork.era = update_data['era']
+            if 'dimensions' in update_data:
+                artwork.dimensions = update_data['dimensions']
+            if 'location' in update_data:
+                artwork.location = update_data['location']
+            if 'image' in update_data:
+                artwork.image = update_data['image']
+            if 'startingbid' in update_data:
+                artwork.startingbid = update_data['startingbid']
+            db.session.commit()
+            return artwork, 200
 
 
 class deleteArtwork(Resource):
     def delete(self, art_id):
+        key = os.getenv('api_key')
+        api_key = request.headers.get('Authorization')
         artExist = ArtworkModel.query.get(art_id)
-        if not artExist:
-            abort(409, message="Artwork id does not exist, cannot delete")
-        db.session.delete(artExist)
-        db.session.commit()
-        return "Artwork deleted", 201
+        if api_key != key:
+            abort(401, message="Invalid API key")
+        else:
+            if not artExist:
+                abort(409, message="Artwork id does not exist, cannot delete")
+            db.session.delete(artExist)
+            db.session.commit()
+            return "Artwork deleted", 201
 
 
 
-api.add_resource(deleteArtwork, '/artwork/<int:art_id>')
+api.add_resource(deleteArtwork, '/artwork/<int:art_id>/')
 api.add_resource(postArtwork, '/artwork/<int:art_id>')
 api.add_resource(putArtwork, '/artwork/<int:art_id>')
 api.add_resource(getArtwork, '/artwork')
